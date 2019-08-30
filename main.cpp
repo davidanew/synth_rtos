477e0554e306ab90cf7a51c462ee9b81679fdef0
David New <46327493+davidanew@users.noreply.github.com>
2019-08-30 09:25:17 +0100

    main.c comment updates

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "main.h"
 
+//Currently thread 1 takes midi command messages from thread 2 and updates the voice array. The voice array is used to construct the waveform and this is sent to the sample buffer
 static void thread1(void *);
+//Thread 2 recieves UART data and processes this to send midi messages to task 1
 static void thread2(void *);
 void SystemClock_Config(void);
-
+//TODO: rename this
+//Queue from task 1 to task to. At the moment it is just a trigger (no information)
 QueueHandle_t queue_handle {nullptr};
 QueueHandle_t uart_byte_queue_handle {nullptr};
 
@@ -11,14 +14,22 @@ int main(void)
 {
 	HAL_Init(); 
 	SystemClock_Config();
+	//Dac1 used to output the waveform
 	Dac_1::init();
+	//Dac2 used for debug/measurement
 	Dac_2::init();
+	//Usart 1 used for MIDI input
 	Usart_1::init();
+	//Usart2 used for virtual com port on PC
 	Usart_2::init();
+	//Tim2 triggers systick which outputs the sample to Dac1
 	Tim::init();
+	//Waves holds precalculated waveform information. The calculation needs to be done before the main tasks start
 	Waves::init();
+	//For checking success of task creation
 	BaseType_t taskCreateReturn;
 	Usart_2::transmit_byte('x');
+	//See queue descriptions above
 	queue_handle = xQueueCreate(10,sizeof(uint8_t));
 	uart_byte_queue_handle = xQueueCreate(10, sizeof(uint8_t));
 	taskCreateReturn = xTaskCreate(thread1, "Add Voice and Calculate Sample", 2048, NULL, 1, NULL);	
@@ -37,6 +48,7 @@ int main(void)
 
 #define NUM_VOICES 1
 
+//Thread 1 needs to know the state of the sample calculation to allow operations on 2 independet message queues (see below for explanation). 
 enum Sample_value_state { calculated, invalid };
 
 //TODO: Rename this, maybe sample_and_state
@@ -53,31 +65,42 @@ static void thread1(void *argument)
 	uint64_t sample_number {0}	;
 	//All voices will be initialised off
 	std::array<Voice, NUM_VOICES> voice_array;
+	//This holds information which is common to all voices
 	Global_parameters global_parameters;
+	//Hold sample value and valididty if sample
 	Sample sample;
 	while (1) {
 		uint8_t queue_message;
+		//Revieve message from thread 2
+		//TODO: define this
 		const BaseType_t xQueueReceiveReturn = xQueueReceive(queue_handle, &queue_message, 0);
 		if (xQueueReceiveReturn == pdTRUE) {
+			//At the moment this just turns one voive on
 			voice_array[0].turn_on(global_parameters, 1000, 1);
 		}	
+		//Sample may still be valid from last loop, but if not calculate another one
 		if(sample.state == invalid) {
-			//we need to calculate a new sample
+			//Running total for the sample value
 			float total {0};
 			*Dac_2::dac2_fast_ptr = 0xFFF;
+			//Loop through voices adding sample value to total
 			for (Voice& voice : voice_array) {
 				total += voice.get_next_sample(sample_number);
 			}
+			//We can now move to the next sample
 			sample_number++;				
 			*Dac_2::dac2_fast_ptr = 0x000;
 			sample.value = total;
 			sample.state = calculated;
 		}
 		//This function should always be run as the sample should have been calculated by calculation above or in previous loop
+		//This buffer send can't block as a message from the input queue may be waiting
+		//If the send is unsuccessful we still have a valid sample for the next loop
 		if (sample.state == calculated) {
-			//attempt to write to buffer
+			//This is a buffer to the ISR that outputs the sample
 			const bool buffer_add_success = Sample_buffer::add_sample(sample.value * (float) 0.5 + (float) 0.5) ; 	
 			if (buffer_add_success)
+				//Need to calcute a new sample
 				sample.state = invalid;
 		}
 		else 
@@ -86,6 +109,11 @@ static void thread1(void *argument)
 	}
 }
 
+//These two funtions are called by Midi_in on success of recieving a full midi message
+//Originally they were implemented as lambdas but std::funtional need dynamic memory allocation
+//These two links have a potential workaround
+//https : //blog.demofox.org/2015/02/25/avoiding-the-performance-hazzards-of-stdfunction/
+//https : //www.quora.com/C++-How-to-pass-a-lambda-function-as-a-parameter-of-another-function
 
 void handle_note_on(Note_on_struct note_on_struct) {
 	uint8_t message {'x'};
@@ -97,6 +125,7 @@ void handle_controller_change(Controller_change_struct controller_change_struct)
 	xQueueSendToFront(queue_handle, &message, portMAX_DELAY);
 } 
 
+//This thread looks for messages from the UART1 IRQ and handles the data in the Midi_in class
 static void thread2(void *argument)
 {
 	(void) argument;
